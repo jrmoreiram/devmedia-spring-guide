fb8ab24ea255d77dd2cf7bd848ed96991ea18724
jmmartins <junior.martins@neogrid.com>
2025-03-31 19:07:19 -0300

    Commit dos projetos referente ao tópico como começar com spring do guia spring.

---
+++ segundo-exemplo-spring/src/main/java/br/com/devmedia/cursosspring/application/ProgramaLembreteEmail.java
@@ -0,0 +1,31 @@
+package br.com.devmedia.cursosspring.application;
+
+import br.com.devmedia.cursosspring.domain.Usuario;
+import br.com.devmedia.cursosspring.service.UsuarioServico;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
+
+@Configuration
+@ComponentScan("br.com.devmedia.cursosspring")
+public class ProgramaLembreteEmail {
+
+    public static void main(String[] args) {
+        Scanner lerDados = new Scanner(System.in);
+        Usuario usuario = new Usuario("Eduardo", "eduardo@exemplo.com", "eduardoDVM");
+
+        System.out.println("Deseja recuperar a senha (S/N)?");
+        String resposta = lerDados.nextLine();
+        if (resposta.equalsIgnoreCase("S")) {
+            ApplicationContext context = new AnnotationConfigApplicationContext(ProgramaLembreteEmail.class);
+            UsuarioServico usuarioServico = context.getBean(UsuarioServico.class);
+
+            usuarioServico.setUsuario(usuario);
+            usuarioServico.lembrarSenhaPorEmail();
+        }
+    }
+
+}
